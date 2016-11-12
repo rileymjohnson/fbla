228def276340e04bfd50cc782b8d9a173478f510
root <rileymillerjohnson@gmail.com>
2016-11-12 15:45:56 +0000

    setup home page and added favicon

--- app/public/views.py
+++ app/public/views.py
@@ -3,7 +3,10 @@ from flask import Blueprint, render_template
 
 blueprint = Blueprint('public', __name__, url_prefix='/', static_folder='../static', template_folder='../templates')
 
-
 @blueprint.route("/")
-def index():
+def indexRoute():
     return render_template("public_pages/index.html")
+
+@blueprint.route("directions")
+def directionsRoute():
+    return render_template("public_pages/directions.html")
--- app/static/public/js/index.js
+++ app/static/public/js/index.js
@@ -13,10 +13,47 @@ $(document).ready(function() {
         navigationHover: false,
         mobileNavHover: false,
         fx: 'simpleFade'
-    })
+    })    
+    
+    //initialize mapbox map
+    mapboxgl.accessToken = 'pk.eyJ1IjoicmlsZXltam9obnNvbiIsImEiOiJjaXY1bGptMzAwMWhtMnluNnd2bzk1cjBzIn0.FicxbRtge8W-_mxFb_Qk1A';
+    var map = new mapboxgl.Map({
+        container: 'google-map',
+        style: 'mapbox://styles/mapbox/streets-v9',
+        center: [-83.51510, 43.02955],
+        zoom: 15
+    });
+    
+    //popup for showing more information
+    var popup = new mapboxgl.Popup({closeOnClick: false, offset: [0, -64]})
+        .setLngLat([-83.51510, 43.02955])
+        .setHTML('<a href="/directions" style="color: black">Directions</a>')
+    
+    // create a DOM element for the marker
+        var el = document.createElement('div');
+        el.className = 'marker';
+        el.style.backgroundImage = 'url(/static/public/images/marker.png)';
+        el.style.width = '42px';
+        el.style.height = '64px';
+    
+    
+        //events for handling mouse hover
+        el.addEventListener("mouseover", function() {
+            el.style.backgroundImage = 'url(/static/public/images/marker_active.png)';
+        })
+        
+        el.addEventListener("mouseout", function() {
+            el.style.backgroundImage = 'url(/static/public/images/marker.png)';
+        })
+        
+        el.addEventListener('click', function() {
+            popup.addTo(map)
+        });
 
-    //initialize google maps
-    $('#google-map').googleMap();
+        // add marker to map
+        new mapboxgl.Marker(el, {offset: [-21, -64]}) //this offset centers the image properly
+            .setLngLat([-83.51510, 43.02955])
+            .addTo(map);
 
     //initialize countdown
     var countdownDate = new Date(2016, 12 - 1, 25);
